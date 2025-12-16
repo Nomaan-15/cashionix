8629cc394be9bb4c865f9c54aa29ac98cb7605dc
Nomaan00 <nomaanmohammed82@gmail.com>
2025-12-16 15:27:59 +0530

    Add admin password reset script

--- cashionix-backend/config/settings.py
+++ cashionix-backend/config/settings.py
@@ -73,9 +73,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600,
+        ssl_require=False,
     )
 }
 
