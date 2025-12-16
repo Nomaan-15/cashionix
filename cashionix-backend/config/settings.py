7056eaba85162b590ddf17fa5a67ab7256bd7abf
Nomaan00 <nomaanmohammed82@gmail.com>
2025-12-16 11:44:01 +0530

    Fix BASE_DIR definition in settings.py

--- cashionix-backend/config/settings.py
+++ cashionix-backend/config/settings.py
@@ -1,6 +1,8 @@
 import os
 import dj_database_url
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-default-key-change-this')
 
