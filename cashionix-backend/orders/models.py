b64c356812943522a3968da2b5b2ea0ccf860457
Nomaan00 <nomaanmohammed82@gmail.com>
2026-01-05 21:04:28 +0530

    made chnages in admin panel

--- cashionix-backend/orders/models.py
+++ cashionix-backend/orders/models.py
@@ -13,7 +13,8 @@ class SellOrder(models.Model):
     final_price = models.IntegerField()
     pickup_address = models.TextField()
     status = models.CharField(max_length=20, default='REQUESTED')
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(null=True, blank=True)
+
     
     # Additional contact fields
     customer_name = models.CharField(max_length=100, blank=True)
